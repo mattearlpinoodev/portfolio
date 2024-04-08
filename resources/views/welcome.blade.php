0ae360427cb53bba168bcff2fdf6c898543b8099
mattearlpinoodev <mattearlpinoo@gmail.com>
2024-04-08 15:07:34 +0800

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -393,18 +393,18 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="{{assets('assets/vendor/purecounter/purecounter_vanilla.js')}}"></script>
-  <script src="{{assets('assets/vendor/aos/aos.js')}}"></script>
-  <script src="{{assets('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-  <script src="{{assets('assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
-  <script src="{{assets('assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
-  <script src="{{assets('assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
-  <script src="{{assets('assets/vendor/typed.js/typed.umd.js')}}"></script>
-  <script src="{{assets('assets/vendor/waypoints/noframework.waypoints.js')}}"></script>
-  <script src="{{assets('assets/vendor/php-email-form/validate.js')}}"></script>
+  <script src="{{asset('assets/vendor/purecounter/purecounter_vanilla.js')}}"></script>
+  <script src="{{asset('assets/vendor/aos/aos.js')}}"></script>
+  <script src="{{asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
+  <script src="{{asset('assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+  <script src="{{asset('assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
+  <script src="{{asset('assets/vendor/typed.js/typed.umd.js')}}"></script>
+  <script src="{{asset('assets/vendor/waypoints/noframework.waypoints.js')}}"></script>
+  <script src="{{asset('assets/vendor/php-email-form/validate.js')}}"></script>
 
   <!-- Template Main JS File -->
-  <script src="{{assets('assets/js/main.js')}}"></script>
+  <script src="{{asset('assets/js/main.js')}}"></script>
 
 </body>
 
